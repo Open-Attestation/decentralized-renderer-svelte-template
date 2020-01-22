57770b1bf2792874cfe618a6eeb948cbc194d983
Nebulis <nebounet@gmail.com>
2020-01-22 17:41:21 +0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  import FramedDocumentRenderer from "./FramedDocumentRenderer.svelte";
+  import Template from "./Template.svelte";
+
+  let templates = [
+    {
+      id: "custom",
+      label: "Custom",
+      template: Template
+    }
+  ];
+  let templateRegistry = {
+    default: templates,
+    custom: templates
+  };
+</script>
+
+<style>
+  body {
+    margin: 0;
+  }
+  main {
+    background-color: #676778;
+    color: white;
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+</style>
+
+<main>
+  <FramedDocumentRenderer {templateRegistry} />
+</main>
---
+++ src/Template.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let document;
+  export let handleObfuscation;
+
+  let handleClick = () => {
+    handleObfuscation("id");
+  };
+</script>
+
+<div>
+  <h1>Hello from svelte</h1>
+  <div>
+    <button on:click={handleClick}>Hide field id</button>
+  </div>
+  <div>{JSON.stringify(document, null, 2)}</div>
+</div>
